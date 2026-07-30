da2667fb1aa0f3a6225bbd86406ec3c9563b97aa
Saroj Sabar <sarojsabar12345@gmail.com>
2026-07-30 22:58:27 +0530

    remov duplicate

---
+++ src/ArraysQuestions/RemoveDuplicateInArrays.java
@@ -0,0 +1,33 @@
+package ArraysQuestions;
+
+import java.util.Arrays;
+
+public class RemoveDuplicateInArrays {
+
+    static int[] removeDuplicate(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+        if (arr.length == 1) {
+            return new int[]{arr[0]};
+        }
+
+        Arrays.sort(arr);
+        int[] result = new int[1];
+        result[0] = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                result = Arrays.copyOf(result, result.length + 1);
+                result[result.length - 1] = arr[i];
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {10,6,8,7,5,5,8,6,9,99,6,1,455,99,0};
+        Arrays.sort(arr);
+        int[] uniqueElements = removeDuplicate(arr);
+        System.out.println(Arrays.toString(uniqueElements));
+    }
+}
