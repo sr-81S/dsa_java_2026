09c025f553d832c5b92f0fae5a9fd104404586dc
Saroj Sabar <sarojsabar12345@gmail.com>
2026-08-05 23:39:23 +0530

    6.Rotate an array right by K positions.

---
+++ src/ArraysQuestions/RotateToKright.java
@@ -0,0 +1,35 @@
+package ArraysQuestions;
+
+import java.util.Arrays;
+
+public class RotateToKright {
+
+    public static void rotateRight(int[] arr, int k){
+        if (arr == null || arr.length <= 1) {
+            return;
+        }
+        k = k % arr.length;
+        if(k == 0) return;
+        rotate(arr, 0, arr.length - 1);
+        rotate(arr, 0, k-1);
+        rotate(arr, k, arr.length-1);
+    }
+
+    public static void rotate (int[] arr, int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        rotateRight(arr, k);
+        System.out.println(Arrays.toString(arr));
+    }
+}
